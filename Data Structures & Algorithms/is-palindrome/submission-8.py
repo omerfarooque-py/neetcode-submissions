7ad73d18f46ce9e6c0215825c7435ea93cac9165
Omer farooque <aghafar2@icloud.com>
2026-04-29 01:31:10 +0500

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L = 0
+        R = len(s)-1
+        while L < R:
+          while L < R and not s[L].isalnum():
+            L+=1
+          while L < R and not s[R].isalnum():
+            R-=1
+          if s[L].lower() != s[R].lower():
+           # print(s[L], s[R])
+            return False
+          R-=1
+          L+=1
+        return True
