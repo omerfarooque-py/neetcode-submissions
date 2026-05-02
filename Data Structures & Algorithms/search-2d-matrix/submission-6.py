98960696e3d107672cf474fb5d4d179edfc81312
Omer farooque <aghafar2@icloud.com>
2026-05-02 19:02:52 +0500

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        R = len(matrix) - 1
+        L = 0
+       
+        
+        while L <= R:
+            mid = L + (R - L) // 2
+
+            if matrix[mid][0] <= target and matrix[mid][-1] >= target:
+                #return matrix[mid] 
+                key_list = matrix[mid]
+                L = 0
+                R = len(key_list) - 1
+                while L <= R:
+                    mid = L + (R - L) // 2
+
+                    if key_list[mid] == target:
+                        return True
+
+                    if key_list[mid] > target:
+                        R = mid - 1
+
+                    elif key_list[mid] < target:
+                        L = mid + 1
+                return False
+            
+            if matrix[mid][0] > target:
+                R = mid -1
+            elif matrix[mid][-1] < target:
+                L = mid + 1
+
+        return False
+                
+
+
+
+                
+        
